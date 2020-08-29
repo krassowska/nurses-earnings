424fdff58fd38f26a7f0b2dc3d7248c8f9c6e3f1
krassowska <gromadzka.karolina@gmail.com>
2020-08-29 18:43:17 +0100

    Create svg map of voivodeships in Poland

--- website/hello.py
+++ website/hello.py
@@ -1,7 +1,25 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    with open("static/maps/wojewodztwa-min.geojson", encoding='utf8') as f:
+        wojewodztwa = json.load(f)["features"]
+    for wojewodztwo in wojewodztwa:
+        #center = wojewodztwo['geometry']['coordinates'][0][0]
+        center_x = 0
+        center_y = 0
+        counter = 0
+        for coodinate in wojewodztwo['geometry']['coordinates'][0]:
+            center_x += coodinate[0]
+            center_y += coodinate[1]
+            counter += 1
+        center_x = center_x / counter
+        center_y = center_y / counter
+        print(center_x, center_y)
+        wojewodztwo["center"] = [center_x, center_y]
+        print(wojewodztwo["center"])
+    return render_template("index.html", wojewodztwa=wojewodztwa)
