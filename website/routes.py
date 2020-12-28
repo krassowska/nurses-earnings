1a07453892a1c79c33406e266a599edac771ab41
krassowska <gromadzka.karolina@gmail.com>
2020-12-28 22:54:10 +0000

    Move courses to a new file

--- website/routes.py
+++ website/routes.py
@@ -4,6 +4,7 @@ from flask import render_template
 
 from app import app
 from map import prepare_regions, prepare_powiats_data
+from courses import education_levels
 
 
 @app.route('/')
@@ -42,96 +43,6 @@ def index():
 
 @app.route('/form')
 def form():
-    specjalizacje = [
-        'Pielęgniarstwo anestezjologiczne i intensywnej opieki',
-        'Pielęgniarstwo chirurgiczne',
-        'Pielęgniarstwo geriatryczne',
-        'Pielęgniarstwo internistyczne',
-        'Pielęgniarstwo onkologiczne',
-        'Pielęgniarstwo operacyjne',
-        'Pielęgniarstwo opieki długoterminowej',
-        'Pielęgniarstwo opieki paliatywnej',
-        'Pielęgniarstwo pediatryczne',
-        'Pielęgniarstwo psychiatryczne',
-        'Pielęgniarstwo ratunkowe',
-        'Pielęgniarstwo rodzinne',
-        'Ochrona zdrowia pracujących',
-        'Pielęgniarstwo epidemiologiczne',
-        'Pielęgniarstwo neonatologiczn',
-        'Pielęgniarstwo ginekologiczno-położnicze',
-        'Pielęgniarstwo rodzinne'
-    ]
-
-    kursy_kwalifikacyjne = [
-        'Pielęgniarstwo anestezjologiczne i intensywnej opieki',
-        'Pielęgniarstwo chirurgiczne',
-        'Pielęgniarstwo diabetologiczne',
-        'Pielęgniarstwo geriatryczne',
-        'Pielęgniarstwo internistyczne',
-        'Pielęgniarstwo kardiologiczne',
-        'Pielęgniarstwo nefrologiczne z dializoterapią',
-        'Pielęgniarstwo neonatologiczne',
-        'Pielęgniarstwo neurologiczne',
-        'Pielęgniarstwo onkologiczne',
-        'Pielęgniarstwo operacyjne',
-        'Pielęgniarstwo opieki długoterminowej',
-        'Pielęgniarstwo opieki paliatywnej',
-        'Pielęgniarstwo pediatryczne',
-        'Pielęgniarstwo psychiatryczne',
-        'Pielęgniarstwo ratunkowe',
-        'Pielęgniarstwo rodzinne',
-        'Pielęgniarstwo środowiska nauczania i wychowania',
-        'Pielęgniarstwo transplantacyjne',
-        'Ochrona zdrowia pracujących',
-        'Pielęgniarstwo epidemiologiczne',
-        'Pielęgniarstwo anestezjologiczne i intensywnej opieki w położnictwie i ginekologii',
-        'Pielęgniarstwo operacyjne',
-        'Pielęgniarstwo rodzinne'
-    ]
-
-    kursy_specjalistyczne = [
-        'Dializoterapia',
-        'Endoskopia',
-        'Kompleksowa opieka pielęgniarska w schorzeniach narządu wzroku',
-        'Kompresjoterapia',
-        'Leczenie ran',
-        'Opieka nad dzieckiem z chorobą nowotworową',
-        'Opieka nad dziećmi i młodzieżą z cukrzycą',
-        'Opieka nad dziećmi ze schorzeniami otorynolaryngologicznymi',
-        'Opieka nad pacjentem poddawanym proc. diag. i terap. z użyciem otwartych źródeł promieniowania',
-        'Opieka nad pacjentem z przewlekłą obturacyjną chorobą płuc (POChP)',
-        'Opieka nad pacjentem ze stomią jelitową',
-        'Pediatryczna domowa opieka paliatywna',
-        'Pielęgnowanie pacjenta dorosłego wentylowanego mechanicznie',
-        'Podstawy opieki paliatywnej',
-        'Rehabilitacja osób z przewlekłymi zaburzeniami psychicznymi',
-        'Szczepienia ochronne',
-        'Wykonanie badania spirometrycznego',
-        'Wykonanie konikopunkcji, odbarczenie odmy prężnej oraz wykonanie dojścia doszpikowego',
-        'Wykonywanie i ocena testów skórnych',
-        'Żywienie dojelitowe i pozajelitowe',
-        'Edukator w cukrzycy',
-        'Komunikowanie interpersonalne w pielęgniarstwie',
-        'Opieka nad osobami z cukrzycą stosującymi terapię ciągłego podskórnego wlewu insuliny CPWI',
-        'Opieka pielęgniarska nad chorymi dorosłymi w leczeniu systemowym nowotworów',
-        'Ordynowanie leków i wypisywanie recept',
-        'Podstawy języka migowego',
-        'Resuscytacja krążeniowo-oddechowa',
-        'Resuscytacja oddechowo-krążeniowa noworodka',
-        'Terapia bólu ostrego u dorosłych',
-        'Terapia bólu przewlekłego u dorosłych',
-        'Wykonanie i interpretacja zapisu elektrokardiograficznego u dorosłych',
-        'Wywiad i badanie fizykalne',
-        'Edukacja i wsparcie kobiety w okresie laktacji',
-        'Leczenie ran',
-        'Monitorowanie dobrostanu płodu w czasie ciąży i podczas porodu',
-        'Onkologia ginekologiczna',
-        'Opieka nad kobietą z cukrzycą w okresie okołoporodowym',
-        'Szczepienia ochronne'
-    ]
-
-    education_levels = {"specjalizacja": specjalizacje, "kurs kwalifikacyjny": kursy_kwalifikacyjne, "kurs specjalistyczny": kursy_specjalistyczne}
-
     courses = {
         level: [
             {
